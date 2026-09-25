5169be57c9c2f08a16b163e6d8d52c6df3bb6040
Rohan <srivastavarohan214@gmail.com>
2026-09-25 19:02:05 +0530

    home page updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -28,20 +28,20 @@ function Home() {
     return (
         <>
 
-            <div className="bg-[#F7EFE8]">
-                <div className="flex justify-center items-center h-screen ">
-                    <img className=" rounded-br-2xl rounded-l-full rounded-tr-full border-5 border-[#2F5E64] "
+            <div className="bg-[#F7EFE8]  ">
+                <div className="flex flex-col sm:flex-row  justify-center items-center h-screen ">
+                    <img className=" rounded-l-full rounded-tr-full border-5  border-[#2F5E64] lg:w-120 lg:h-80 w-100 h-60 "
                         src="https://img.freepik.com/free-photo/smiley-woman-with-headphones-working-laptop_23-2148764530.jpg?t=st=1751754978~exp=1751758578~hmac=19ec2661a5f56da11ab229bf6626dfedd7a557b4cbfe8e2a1b8160aa4505a61f%22"
                         alt=""
                     />
                     <div className="text-end" >
-                        <div className="text-8xl font-semibold mb-5 items-start text-[#2F5E64]">Write your
+                        <div className="text-6xl lg:text-8xl font-semibold mb-5 items-start text-[#2F5E64]">Write your
                             <span className="text-[#BC7E6C]"><br />Article<br /></span>
-                            <Link to="/addArticles" className="text-[#BC7E6C] text-8xl font-semibold underline-offset-10 underline hover:text-[#2F5E64]">here</Link>
+                            <Link to="/addArticles" className="text-[#BC7E6C] lg:text-8xl font-semibold underline-offset-10 underline hover:text-[#2F5E64]">here</Link>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> 
         </>
     );
 }
